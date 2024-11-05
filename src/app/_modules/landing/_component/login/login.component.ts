5b3308ae0a4c90ca0bf27baa9ce1c946fd58ac42
Ravi Kumar <ravi.kumar2@daffodilsw.com>
2024-11-05 18:14:43 +0530

    ui

--- src/app/_modules/landing/_component/login/login.component.ts
+++ src/app/_modules/landing/_component/login/login.component.ts
@@ -3,6 +3,7 @@ import { AuthService } from '../../../../_services/auth/auth.service';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { SnackbarService } from '../../../../_services/snackbar/snackbar.service';
 import { Router } from '@angular/router';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'fin-login',
@@ -17,12 +18,13 @@ export class LoginComponent implements OnInit {
     private authService: AuthService,
     private fb: FormBuilder,
     private snackbarService: SnackbarService,
-    private router: Router
+    private router: Router,
+    private matDialogRef: MatDialogRef<LoginComponent>
   ) { }
 
   initForm() {
     this.form = this.fb.group({
-      username: [''],
+      userid: [''],
       password: ['']
     });
   }
@@ -36,6 +38,7 @@ export class LoginComponent implements OnInit {
       next: (res: any) => {
         if (res) {
           this.snackbarService.successSnackbar('Login Successful');
+          this.matDialogRef.close();
           this.navigateToDashboard();
         }
       },
---
+++ src/app/_modules/services/_components/service-header/service-header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fin-service-header',
+  templateUrl: './service-header.component.html',
+  styleUrl: './service-header.component.scss'
+})
+export class ServiceHeaderComponent {
+
+}
